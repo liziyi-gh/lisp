0ea3afe90b4b6c24227b3117f22cb7d6c63aef68
liziyi <makisemengjun@gmail.com>
2021-10-16 17:33:38 +0800

    for sync

--- interpreter.py
+++ interpreter.py
@@ -1,9 +1,16 @@
+import re
 import math
 import operator
 
 """
 There a functional way to do this
 """
+
+def car(x):
+    return x[0]
+
+def cdr(x):
+    return x[1:]
 
 standard_env = {
     '+': operator.add,
@@ -20,10 +27,32 @@ standard_env = {
 def apply():
     pass
 
-def eval(exp, env=standard_env):
-    pass
+def eval(exp, env=standard_env, stack_depth=0):
+    if exp[0] == '(':
+        tmp = exp[1]
+        if env['number?'](tmp):
+            # TODO float complex support
+            return int(tmp)
+        if env['symbol?'](tmp):
+            # TODO handle error
+            return env(tmp)
 
-def parse(exp):
-    pass
+def parse(exp:str):
+    # TODO see python3 cook book tokens
+    # TODO more pythonic
+    # TODO exp should be one line format
+    tokens = []
+    tmp = ""
+    for i in range(len(exp)):
+        if exp[i] == '(' or exp[i] == ')':
+            tokens.append(exp[i])
+        if exp[i] == ' ':
+            tokens.append(tmp)
+            tmp = ""
+        else:
+            tmp += exp[i]
+    return tokens
 
-a = [1, 2, 3]
+
+source = "(+ 1 2 (- 2 3) (- 3 3))"
+print(parse(source))
