a522d50048cae9a2a6ef75f5fbf83d21376555a1
liziyi <gg_liziyi@qq.com>
2021-11-27 17:11:20 +0800

    make it to block

--- interpreter.py
+++ interpreter.py
@@ -1,5 +1,14 @@
 import operator
 
+class Block():
+    def __init__(self):
+        self.exp = ''
+        self.child = []
+
+    def add_child(self, exp):
+        self.child.append(exp)
+
+
 def car(x):
     return x[0]
 
@@ -32,11 +41,6 @@ standard_env = {
     'symbol?':lambda x: isinstance(x, str),
 }
 
-def get_nth_region(exp, n:int):
-    pass
-
-
-
 def apply(env, exp):
     func = env[exp[0]]
     if func is standard_env['+']:
@@ -54,9 +58,31 @@ def eval(exp, env=standard_env):
         else:
             return env(tmp)
 
+def find_next_brackets(tokens, begin)->int:
+    stack = 0
+    while begin<len(tokens):
+        if tokens[begin] == '(':
+            stack += 1
+        if tokens[begin] == ')':
+            stack -= 1
+            if stack == 0:
+                return begin
+        begin += 1
+    return -1
+
 
 def parse_tokens(tokens):
-    pass
+    root = Block()
+    i = 0
+    j = 0
+    while(i<len(tokens)):
+        if tokens[i] == '(':
+            j = find_next_brackets(tokens, i)
+            root.exp = tokens[i:j+1]
+            root.add_child(parse_tokens(tokens[i+1:j]))
+            i = j
+        i = i+1
+    return root
 
 def parse(exp:str):
     # TODO: exp has to be one line format now
@@ -80,6 +106,6 @@ def parse(exp:str):
 
 source = "(+ 1 (+ 2 1))"
 tokens = parse(source)
-blocks = parse_tokens(tokens)
+syntax_tree = parse_tokens(tokens)
 print(tokens)
-print(eval(tokens))
+# print(eval(tokens))
