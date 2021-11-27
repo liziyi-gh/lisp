25da83405ec667aa074fc2b545327a3e1974035d
liziyi <gg_liziyi@qq.com>
2021-11-27 16:18:02 +0800

    I should use block tree

--- interpreter.py
+++ interpreter.py
@@ -1,10 +1,4 @@
-import re
-import math
 import operator
-
-"""
-There a functional way to do this
-"""
 
 def car(x):
     return x[0]
@@ -12,47 +6,80 @@ def car(x):
 def cdr(x):
     return x[1:]
 
+def is_number(x):
+    try:
+        return int(x)
+    except ValueError:
+        pass
+    try:
+        return float(x)
+    except ValueError:
+        pass
+    try:
+        return complex(x)
+    except:
+        pass
+
 standard_env = {
     '+': operator.add,
     '-': operator.sub,
-    'equal?': operator.eq,
-    'eq?': operator.is_,
+    # 'equal?': operator.eq,
+    # 'eq?': operator.is_,
     'car': lambda x: x[0],
     'cdr': lambda x: x[1:],
     'cons': lambda x, y: [x, y],
-    'number?': lambda x: isinstance(x, (int, float, complex)),
+    'number?': is_number,
     'symbol?':lambda x: isinstance(x, str),
 }
 
-def apply():
+def get_nth_region(exp, n:int):
     pass
 
-def eval(exp, env=standard_env, stack_depth=0):
-    if exp[0] == '(':
-        tmp = exp[1]
-        if env['number?'](tmp):
-            # TODO float complex support
-            return int(tmp)
-        if env['symbol?'](tmp):
-            # TODO handle error
+
+
+def apply(env, exp):
+    func = env[exp[0]]
+    if func is standard_env['+']:
+        return func(eval(exp[1]), eval(exp[2]))
+
+
+def eval(exp, env=standard_env):
+    tmp = exp[1] if exp[0] == '(' else exp[0]
+    if env['number?'](tmp):
+        # TODO: float complex support
+        return int(tmp)
+    if env['symbol?'](tmp):
+        if hasattr(env[tmp], '__call__'):
+            return apply(env, exp[1:-1])
+        else:
             return env(tmp)
 
+
+def parse_tokens(tokens):
+    pass
+
 def parse(exp:str):
-    # TODO see python3 cook book tokens
-    # TODO more pythonic
-    # TODO exp should be one line format
+    # TODO: exp has to be one line format now
     tokens = []
     tmp = ""
     for i in range(len(exp)):
-        if exp[i] == '(' or exp[i] == ')':
+        if exp[i] == '(':
             tokens.append(exp[i])
-        if exp[i] == ' ':
+            tmp=''
+        elif exp[i] == ')':
+            if exp[i-1] != ')':
+                tokens.append(tmp)
+            tmp=''
+            tokens.append(')')
+        elif exp[i] == ' ':
             tokens.append(tmp)
-            tmp = ""
+            tmp = ''
         else:
             tmp += exp[i]
     return tokens
 
-
-source = "(+ 1 2 (- 2 3) (- 3 3))"
-print(parse(source))
+source = "(+ 1 (+ 2 1))"
+tokens = parse(source)
+blocks = parse_tokens(tokens)
+print(tokens)
+print(eval(tokens))
