fc7a16f326d21cbaf80505384333c3f782888750
liziyi <gg_liziyi@qq.com>
2021-12-16 14:11:27 +0800

    feat: add profile.py
    1.add profile.py
    Author:liziyi

---
+++ tool/profile.py
@@ -0,0 +1,12 @@
+import time
+from functools import wraps
+
+def timethis(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.process_time()
+        r = func(*args, **kwargs)
+        end = time.process_time()
+        print('{}.{} : {}'.format(func.__module__, func.__name__, end - start))
+        return r
+    return wrapper
